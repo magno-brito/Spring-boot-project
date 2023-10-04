f3490ef01a04009ffe57c3476579cec0380dbe7f
Magno Leal de Brito Junior <CIANDT\magnobrito@lnb024243cps.ciandt.global>
2023-10-04 19:52:20 -0300

    Creating authentication class

--- mywebapp/src/main/java/com/parte2/mywebapp/hello/LoginController.java
+++ mywebapp/src/main/java/com/parte2/mywebapp/hello/LoginController.java
@@ -1,10 +1,12 @@
 package com.parte2.mywebapp.hello;
 
+import com.parte2.mywebapp.authentication.AuthenticationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 //atalho para reformatar CTRL + ALT + L
@@ -13,13 +15,39 @@ public class LoginController {
 
     //Creating the variable logger to show the loggs
     private Logger logger = LoggerFactory.getLogger(getClass());
-    @RequestMapping("login")
+
+    public LoginController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    private AuthenticationService authenticationService;
+
+    @RequestMapping("loginExemple")
     public String logingHello(@RequestParam String name, ModelMap model) {
         model.put("name", name);
         logger.debug("Debug - Request param is {}", name);
         logger.info("Info - Request param is  {}", name);
 //        System.out.println("Request param is " + name); //Not a good practice
         return "loginHello";
+    }
+
+    //This method deals with POST and GET request
+    @RequestMapping(value = "login", method = RequestMethod.GET)
+    public String gotoLogin() {
+        return "loginHello";
+    }
+
+    @RequestMapping(value = "login", method = RequestMethod.POST)
+    public String goToWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model) {
+
+        if (authenticationService.authenticate(name, password)) {
+            model.put("name", name);
+            model.put("password", password);
+            return "welcome";
+        }
+        model.put("errorMessage", "Invalid user of password");
+        return "loginHello";
+
     }
 
 
